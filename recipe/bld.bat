c8d4182a1b40d632cdc5d0bd3a46bddb632bc675
Michael Sarahan <msarahan@gmail.com>
2017-08-01 12:38:25 -0500

    simplify recipe using conda-build 3

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,6 @@
-"%PYTHON%" setup.py configure --hdf5="%LIBRARY_PREFIX%"  --hdf5-version=1.8.18
+:: hdf5 env var provided by conda-build 3 variant
+python setup.py configure --hdf5="%LIBRARY_PREFIX%"  --hdf5-version=%hdf5%
 if errorlevel 1 exit 1
 
-"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
+python setup.py install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
