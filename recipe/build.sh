a70e0997a3c37d3f4b8e402997f64da1d92158a1
Tim Greaves <tim.greaves@imperial.ac.uk>
2020-05-07 10:49:25 +0100

    Hardcode ICHPC dependencies

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-"${PYTHON}" setup.py configure --hdf5="${PREFIX}"
+"${PYTHON}" setup.py configure --mpi --hdf5-version=1.10.0 --hdf5="/rds/general/project/software-ese/live/libraries/ese-hdf5/1.10.6-gcc7.5.0"
 "${PYTHON}" -m pip install . --no-deps --ignore-installed -vvv
