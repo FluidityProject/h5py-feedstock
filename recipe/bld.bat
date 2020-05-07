eceb96aa90a909b9461457a5251f136f8e6dde71
Tim Greaves <tim.greaves@imperial.ac.uk>
2020-05-07 10:54:14 +0100

    Localise for IC HPC

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-"%PYTHON%" setup.py configure --hdf5="%LIBRARY_PREFIX%"  --hdf5-version=%hdf5%
+"%PYTHON%" setup.py configure --mpi --hdf5="/rds/general/project/software-ese/live/libraries/ese-hdf5/1.10.6-gcc7.5.0"  --hdf5-version="1.10.6"
 if errorlevel 1 exit 1
 
 "%PYTHON%" -m pip install . --no-deps --ignore-installed -vvv
